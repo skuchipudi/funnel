04193a4bde44bcc41669cd1a0086264f720ebded
spntr <sunil_kuchipudi@yahoo.com>
2016-09-13 08:14:07 -0400

    new file:   app/schedule.transfer.component.html 	new file:   app/schedule.transfer.component.ts

---
+++ app/schedule.transfer.component.ts
@@ -0,0 +1,21 @@
+import {Component} from 'angular2/core';
+import {Router,CanDeactivate} from 'angular2/router';
+
+
+@Component({
+    templateUrl: '/app/schedule.transfer.component.html'
+})
+
+export class ScheduleTransferComponent {
+    
+   constructor (private _router: Router){
+
+   }
+    
+    onSubmit(form :any){
+        console.log(form);
+        this._router.navigate (['Albums']);
+    }
+
+  
+}
