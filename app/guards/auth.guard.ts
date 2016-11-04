73ae01ba25c98ef5a430cef9badf5b826eae482d
Sunil Kuchipudi <sunil@guardianinformatics.com>
2016-11-03 22:49:03 -0400

    new file:   app/guards/auth.guard.js 	new file:   app/guards/auth.guard.js.map 	new file:   app/guards/auth.guard.ts 	new file:   app/guards/index.js 	new file:   app/guards/index.js.map 	new file:   app/guards/index.ts 	new file:   app/login/index.js 	new file:   app/login/login.component.js 	new file:   app/login/login.component.js.map

---
+++ app/guards/auth.guard.ts
@@ -0,0 +1,30 @@
+﻿import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { tokenNotExpired } from 'angular2-jwt';
+
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+    constructor(private router: Router) { }
+
+    canActivate() {
+        console.log("AUTHGUARD -- canActivate() called");
+          
+       // if (localStorage.getItem('id_token')) {
+            // logged in so return true
+       //     return true;
+        //}
+       console.log( "AuthGuard.tokenNotExpired()" + tokenNotExpired());
+
+        if(tokenNotExpired())
+        {
+           console.log("tokenNotExpired" + tokenNotExpired())
+           return true;
+        }
+        // not logged in so redirect to login page
+        //this.router.navigate(['/login']);
+         this. router.navigate(['/filesentinelsplash']);
+            return false;
+    }
+}
