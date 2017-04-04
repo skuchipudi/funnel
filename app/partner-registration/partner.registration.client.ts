9827861906bb0692933a619aa91f7cc0526dc1b8
Sunil Kuchipudi <sunil@guardianinformatics.com>
2017-04-03 22:45:07 -0400

    modified:   app/partner-detail/partner.detail.component.html 	modified:   app/partner-registration/partner.registration.client.js.map 	modified:   app/partner-registration/partner.registration.client.ts

--- app/partner-registration/partner.registration.client.ts
+++ app/partner-registration/partner.registration.client.ts
@@ -1,4 +1,7 @@
 export class Client
 {
-	 constructor(public clientName: string, public clientDescription: string, public emailAddress: any) { }
+	 constructor(
+		public clientName: string, 
+	 	public clientDescription: string, 
+	 	public emailAddress: any) { }
 }
