69662a06430edb5a546ba5f691dd0257e8db66ef
Sunil Kuchipudi <sunil@guardianinformatics.com>
2017-09-17 08:15:36 -0400

    new file:   partner-delete/partner.delete.confirm.component.js 	new file:   partner-delete/partner.delete.confirm.component.js.map 	new file:   partner-delete/partner.delete.confirm.component.ts 	new file:   partner-delete/partner.delete.services.js 	new file:   partner-delete/partner.delete.services.js.map 	new file:   partner-delete/partner.delete.services.ts

---
+++ app/partner-delete/partner.delete.services.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+declare var jQuery: any;
+declare var partnerstable:any;
+declare var table:any;
+
+
+@Injectable()
+export class PartnerDeleteServices {
+    private partner_delete_url =
+         'http://localhost:8080/partnerservices/delete/';
+
+    constructor(private _http: Http) { }
+
+   
+    
+    deletePartner(partnerId: any) {
+          console.log (this.partner_delete_url+partnerId);
+         return this._http.get(this.partner_delete_url+partnerId)
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
+
+    handleError(error: Response){
+        console.error(error);
+        return  Observable.throw(error.json().error || 'Server Error');
+    }
+
+    
+
+
+}
